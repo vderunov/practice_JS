b83ffbe3c1c126c995ec70984ba8857dcb3eb3a9
Volodymyr_Derunov <vderunov@gmail.com>
2019-02-18 03:50:12 +0200

    Create HW_6

---
+++ HW_6/layout/assets/main.js
@@ -0,0 +1,12 @@
+function togglemenu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Look for .hamburger
+var hamburger = document.querySelector(".hamburger");
+// On click
+hamburger.addEventListener("click", function () {
+    // Toggle class "is-active"
+    hamburger.classList.toggle("is-active");
+    // Do something else, like open/close menu
+});
